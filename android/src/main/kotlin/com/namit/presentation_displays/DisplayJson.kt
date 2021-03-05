2fc0d497b385ab66705a6ff66b1874ce2d9e28b9
Sean <sean@linkpos.com.au>
2021-03-05 18:49:59 +1100

    fix Gson release issue

--- android/src/main/kotlin/com/namit/presentation_displays/DisplayJson.kt
+++ android/src/main/kotlin/com/namit/presentation_displays/DisplayJson.kt
@@ -1,8 +1,11 @@
 package com.namit.presentation_displays
 
+import androidx.annotation.Keep;
+
 /**
  * @hide
  */
+@Keep
 data class DisplayJson(val displayId: Int,
                        val flags: Int,
                        val rotation: Int,
